3351c7a7c12447fef241e8ef3e1cde7c93bd690e
Alden Keefe Sampson <aldenkeefesampson@gmail.com>
2012-04-14 02:53:18 -0400

    ajaxified form submit

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,14 @@
 jQuery(function ($) {
+
     $('#show').autocomplete({
         source: "show/auto"
+    });
+
+    //ajaxify the post submit
+    var AJAXify = true;
+    $('form').submit(function (e) {
+        if (!AJAXify) return;
+        e.preventDefault();
+        $.post( $(this).attr('action') , $(this).serialize() );
     });
 });
