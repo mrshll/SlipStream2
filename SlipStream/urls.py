38319922a288b26263fab1cc895c0ec8463ac937
Alden Keefe Sampson <aldenkeefesampson@gmail.com>
2012-04-14 01:02:00 -0400

    fixed static files

--- SlipStream/urls.py
+++ SlipStream/urls.py
@@ -1,13 +1,21 @@
 from django.conf.urls import patterns, include, url
+from SlipStream import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'show.views.home', name='home'),
-    # url(r'^SlipStream/', include('SlipStream.foo.urls')),
+        # show
+        url(r'^$', 'show.views.home', name='home'),
 
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+        # static/media routes
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root':settings.STATIC_ROOT}),
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root':settings.MEDIA_ROOT}),
+
+        # admin
+        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+        url(r'^admin/', include(admin.site.urls)),
 )
