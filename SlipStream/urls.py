9c4fddbabe4c907098ed23b3efeff0e917e7c9a4
Marshall <mmoutenot@gmail.com>
2012-04-14 00:56:39 -0400

    fixed admin in urls

--- SlipStream/urls.py
+++ SlipStream/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'show.views.home', name='home'),
